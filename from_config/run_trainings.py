5cf8a4d4fa6976bb9b7118e4a5fc25108092ef7a
Christian Kragh Jespersen <chri862z@hep04.cluster>
2021-03-26 14:48:39 +0100

    finished work on n-hop (SGConv in from_config_dev)

--- from_config/run_trainings.py
+++ from_config/run_trainings.py
@@ -12,7 +12,7 @@ if len(gpu_devices) > 0:
     for i in range(len(gpu_devices)):
         tf.config.experimental.set_memory_growth(gpu_devices[i], True)
 
-exp_folder = str(sys.argv[1])
+exp0_folder = str(sys.argv[1])
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
 SHUTDOWN = False
 ##########################################################
@@ -22,8 +22,8 @@ SHUTDOWN = False
 # Generate list over experiments to run
 from dev.utils import list_experiments, clean_done, check_dataset
 from dev.train_script import train_model
-clean_done(exp_folder)
-exp_folder, exp_list = list_experiments(exp_folder)
+clean_done(exp0_folder)
+exp_folder, exp_list = list_experiments(exp0_folder)
 
 print(f"Starting process with {len(exp_list)} experiments")
 print(exp_list)
@@ -46,7 +46,7 @@ for i, experiment in enumerate(exp_list):
 
     epochexit=train_model(construct_dict)
     print(f'Exited training after {epochexit} epochs')
-    shutil.move(osp.join(exp_folder, experiment), osp.join("experiments/done", experiment))
+    shutil.move(osp.join(exp_folder, experiment), osp.join(exp0_folder+"/done", experiment))
     print(f"Experiment {experiment[:-5]} done \t {experiment}: {i + 1} / {len(exp_list)}")
 
 
