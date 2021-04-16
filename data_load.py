ed57aca88bd510a988f58d040655d3b9fdff2d42
astrockragh <christian@kraghjespersen.dk>
2021-04-16 18:27:31 +0200

    gradient importance first try

--- data_load.py
+++ data_load.py
@@ -1,5 +1,5 @@
 import numpy as np
-import os, sqlite3, pickle, sys, gzip, shutil, time
+import os, sqlite3, pickle, sys, gzip, shutil
 if hasattr(__builtins__,'__IPYTHON__'):
     print('Notebook')
     from tqdm.notebook import tqdm
@@ -15,44 +15,50 @@ import matplotlib.pyplot as plt
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
 from spektral.data import Dataset, Graph
-import tensorflow as tf
 from scipy.sparse import csr_matrix
 
-features = ["dom_x", "dom_y", "dom_z", "time", "charge_log10"]
+# features = ["dom_x", "dom_y", "dom_z", "time", "charge_log10", "SRTInIcePulses"]
 target_angle = ["energy_log10", "zenith","azimuth"]
-target_unit  = ["energy_log10", "direction_x", "direction_y", "direction_z"]
-target_pos  = ["energy_log10", "position_x", "position_y", "position_z", "direction_x", "direction_y", "direction_z"]
+target_unitvec  = ["energy_log10", "direction_x", "direction_y", "direction_z"]
 
 class graph_data(Dataset):
     """
-    First hopefully working version of the data
+    data that takes config file
     """
 
-    def __init__(self, n_data = None, transform=True, unitvec=True, angle=False, pos=False, muon = True, skip = 0, n_neighbors = 6, restart=False, dom_norm=1e3, train_val_test_split = [0.8, 0.1, 0.1], **kwargs):
-        self.n_data = n_data
-        self.skip   = skip
-        self.dom_norm=dom_norm
-        self.n_neighbors = n_neighbors
-        self.unitvec=unitvec
-        self.angle=angle
-        self.pos=pos
-        if sum(train_val_test_split) != 1:
-            sys.exit("Total splits must add up to 1")
-        self.train_size, self.val_size, self.test_split = train_val_test_split
-        self.seed = 42
-        self.transform=transform
-        self.restart=restart
+    def __init__(self, n_data = 1 ,features=["dom_x", "dom_y", "dom_z", "time", "charge_log10", "SRTInIcePulses"], targets= ["energy_log10", "zenith","azimuth"], muon = True, skip = 0,\
+        transform_path='db_files/muongun/transformers.pkl',\
+             db_path= 'db_files/muongun/rasmus_classification_muon_3neutrino_3mio.db',\
+                  n_neighbors = 6, restart=False, data_split = [0.8, 0.1, 0.1], SRT=0, graph_construction='classic', database='MuonGun', **kwargs):
+
+
+        self.n_data = int(n_data)
+        self.features=features
+        self.targets=targets
         self.muon=muon
+        self.skip   = skip
+        self.dom_norm = 1e3
+        self.transform_path=transform_path
+        self.db_path=db_path
+        self.n_neighbors = n_neighbors
+        if sum(data_split) != 1:
+            sys.exit("Total splits must add up to 1")
+        self.train_size, self.val_size, self.test_split = data_split
+        self.seed = 42
+        self.restart=restart
+        self.SRT=SRT
+        self.graph_construction=graph_construction
+        self.database=database
         self.k=0
         super().__init__(**kwargs)
-
+    
     @property
     def path(self):
         """
         Set the path of the data to be in the processed folder
         """
         cwd = osp.abspath('')
-        path = osp.join(cwd, "processed/graph_dataset")
+        path = osp.join(cwd, f"processed/{self.database}_muon_{self.muon}_n_data_{self.n_data}_type_{self.graph_construction}")
         return path
 
     def reload(self):
@@ -60,52 +66,49 @@ class graph_data(Dataset):
             shutil.rmtree(self.path)
             print('Removed and ready to reload')
 
+    def check_dataset(self):
+        return osp.exists(self.path)
+
     def download(self):
-        download_start = time.time()
-        # Get raw_data
-        db_folder = osp.join(osp.abspath(''), "db_files")
-        db_file   = osp.join(db_folder, "rasmus_classification_muon_3neutrino_3mio.db")
+        if not self.check_dataset():
+            # Get raw_data
+            db_file   = self.db_path
 
-        # Make output folder
-        os.makedirs(self.path)
+            # Make output folder
+            os.makedirs(self.path)
 
-        print("Connecting to db-file")
-        with sqlite3.connect(db_file) as conn:
-            # Find indices to cut after
-            try:
-                if self.muon:
-                    print('Loading Muons')
-                    start_id = conn.execute(f"select distinct event_no from features where event_no>=138674340 limit 1 offset {self.skip}").fetchall()[0][0]
-                    stop_id  = conn.execute(f"select distinct event_no from features where event_no>=138674340 limit 1 offset {self.skip + self.n_data}").fetchall()[0][0]
-                else:
-                    print('Loading Neutrinos')
-                    start_id = conn.execute(f"select distinct event_no from features limit 1 offset {self.skip}").fetchall()[0][0]
-                    stop_id  = conn.execute(f"select distinct event_no from features limit 1 offset {self.skip + self.n_data}").fetchall()[0][0]
-            except:
-                ""
-                start_id = 0
-                stop_id  = 999999999
-            if self.unitvec:
-                targets=target_unit
-            if self.angle:
-                targets=target_angle 
-            if self.pos:
-                targets=target_pos
-            if self.unitvec==self.angle==1 or self.unitvec==self.pos==1 or self.pos==self.angle==1:
-                print('Multiple selections made')
+            print("Connecting to db-file")
+            with sqlite3.connect(db_file) as conn:
+                # Find indices to cut after
+                try:
+                    if self.muon:
+                        print('Loading Muons')
+                        start_id = conn.execute(f"select distinct event_no from features where event_no>=138674340 limit 1 offset {self.skip}").fetchall()[0][0]
+                        stop_id  = conn.execute(f"select distinct event_no from features where event_no>=138674340 limit 1 offset {self.skip + self.n_data}").fetchall()[0][0]
+                    else:
+                        print('Loading Neutrinos')
+                        start_id = conn.execute(f"select distinct event_no from features limit 1 offset {self.skip}").fetchall()[0][0]
+                        stop_id  = conn.execute(f"select distinct event_no from features limit 1 offset {self.skip + self.n_data}").fetchall()[0][0]
+                except:
+                    ""
+                    start_id = 0
+                    stop_id  = 100
+                # SQL queries format
+                feature_call = ", ".join(self.features)
+                target_call  = ", ".join(self.targets)
                 
-            # SQL queries format
-            feature_call = ", ".join(features)
-            target_call  = ", ".join(targets)
-
-            # Load data from db-file
-            print("Reading files")
-            df_event = read_sql(f"select event_no       from features where event_no >= {start_id} and event_no < {stop_id} and SRTInIcePulses = 1", conn)
-            df_feat  = read_sql(f"select {feature_call} from features where event_no >= {start_id} and event_no < {stop_id} and SRTInIcePulses = 1", conn)
-            df_targ  = read_sql(f"select {target_call } from truth    where event_no >= {start_id} and event_no < {stop_id}", conn)
-            
-            if self.transform:
-                transformers = pickle.load(open(osp.join(db_folder, "transformers.pkl"), 'rb'))
+                # Load data from db-file
+                print("Reading files")
+                if self.SRT:
+                    df_event = read_sql(f"select event_no       from features where event_no >= {start_id} and event_no < {stop_id} and SRTInIcePulses = 1", conn)
+                    df_feat  = read_sql(f"select {feature_call} from features where event_no >= {start_id} and event_no < {stop_id} and SRTInIcePulses = 1", conn)
+                    df_targ  = read_sql(f"select {target_call } from truth    where event_no >= {start_id} and event_no < {stop_id}", conn)
+                else:
+                    df_event = read_sql(f"select event_no       from features where event_no >= {start_id} and event_no < {stop_id}", conn)
+                    df_feat  = read_sql(f"select {feature_call} from features where event_no >= {start_id} and event_no < {stop_id}", conn)
+                    df_targ  = read_sql(f"select {target_call } from truth    where event_no >= {start_id} and event_no < {stop_id}", conn)
+                
+                transformers = pickle.load(open(self.transform_path, 'rb'))
                 trans_x      = transformers['features']
                 trans_y      = transformers['truth']
 
@@ -119,36 +122,37 @@ class graph_data(Dataset):
             
             
 
-            # Cut indices
-            print("Splitting data to events")
-            idx_list    = np.array(df_event)
-            x_not_split = np.array(df_feat)
+                # Cut indices
+                print("Splitting data to events")
+                idx_list    = np.array(df_event)
+                x_not_split = np.array(df_feat)
 
-            _, idx, counts = np.unique(idx_list.flatten(), return_index = True, return_counts = True) 
-            xs          = np.split(x_not_split, np.cumsum(counts)[:-1])
+                _, idx, counts = np.unique(idx_list.flatten(), return_index = True, return_counts = True) 
+                xs          = np.split(x_not_split, np.cumsum(counts)[:-1])
 
-            ys          = np.array(df_targ)
-            print(df_feat.head())
-            print(df_targ.head())
+                ys          = np.array(df_targ)
+                print(df_feat.head())
+                print(df_targ.head())
 
-            # Generate adjacency matrices
-            print("Generating adjacency matrices")
-            graph_list = []
-            for x, y in tqdm(zip(xs, ys), total = len(xs)):
-                try:
-                    a = knn(x[:, :3], self.n_neighbors)
-                except:
-                    a = csr_matrix(np.ones(shape = (x.shape[0], x.shape[0])) - np.eye(x.shape[0]))
-
-
-                graph_list.append(Graph(x = x, a = a, y = y))
-
-            graph_list = np.array(graph_list, dtype = object)
+                # Generate adjacency matrices
+                print("Generating adjacency matrices")
+                graph_list = []
+                for x, y in tqdm(zip(xs, ys), total = len(xs)):
+                    try:
+                        a = knn(x[:, :3], self.n_neighbors)
+                    except:
+                        a = csr_matrix(np.ones(shape = (x.shape[0], x.shape[0])) - np.eye(x.shape[0]))
 
 
-            print("Saving dataset")
-            pickle.dump(graph_list, open(osp.join(self.path, "data.dat"), 'wb'))
+                    graph_list.append(Graph(x = x, a = a, y = y))
 
+                graph_list = np.array(graph_list, dtype = object)
+
+
+                print("Saving dataset")
+                pickle.dump(graph_list, open(osp.join(self.path, "data.dat"), 'wb'))
+        else:
+            pass
         
     def read(self):
         if self.restart and self.k==0:
